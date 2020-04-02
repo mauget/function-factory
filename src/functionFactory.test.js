116ccf7f23d2f60d1f108ec5322370d3126ca8f9
mauget <mauget@gmail.com>
2020-04-02 14:40:45 -0400

    Increase coverage

--- src/functionFactory.test.js
+++ src/functionFactory.test.js
@@ -10,6 +10,20 @@ describe('factory dispatch of code', () => {
         expect(result.includes(param));
     });
 
+    test(`"Two" result contains "Two" and "${param}"`, () => {
+        const result = functionFactory('Two', param);
+
+        expect(result.includes('Two'));
+        expect(result.includes(param));
+    });
+
+    test(`"One" result contains "Three" and "${param}"`, () => {
+        const result = functionFactory('Three', param);
+
+        expect(result.includes('Three'));
+        expect(result.includes(param));
+    });
+
     test(`"BAD" result contains "BAD", and "not registered" (but not "${param}")`, () => {
         const result = functionFactory('BAD', param);
 
